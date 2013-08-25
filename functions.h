d9470c42c346ef03735b3ae48de8a91cb43add2a
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2013-08-25 18:23:01 +0100

    Make SUPPRESS_ERROR_OUTPUT macro more compact.
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

--- functions.h
+++ functions.h
@@ -54,15 +54,15 @@ extern "C" {
 # define ENOSYS 38
 #endif
 
-#define SUPPRESS_ERROR_OUTPUT(name, result, ...)                        \
-    do {                                                                \
-        int __##name##_result;                                          \
-        save_t __##name##_save;                                         \
-        __##name##_result = suppress_error_output(&(__##name##_save));  \
-        result = name(__VA_ARGS__);                                     \
-        if (!(__##name##_result)) {                                     \
-            restore_error_output(&__##name##_save);                     \
-        }                                                               \
+#define SUPPRESS_ERROR_OUTPUT(s, x, ...)            \
+    do {                                            \
+        int __##s;                                  \
+        save_t __s_##s;                             \
+        __##s = suppress_error_output(&(__s_##s));  \
+        x = s(__VA_ARGS__);                         \
+        if (!(__##s)) {                             \
+            restore_error_output(&__s_##s);         \
+        }                                           \
     } while(0)
 
 extern int errno;
