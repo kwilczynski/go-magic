b951df2d5c9326fd860a0761ef1015d38a8d6ec7
Krzysztof Wilczynski <kw@linux.com>
2018-12-30 05:33:57 +0100

    Fix tests so that older versions of libmagic are also covered.
    This commit builds upon changes in 638486d77a4e6672a3d3168295dbc4fd2ef739d2.
    
    Signed-off-by: Krzysztof Wilczynski <kw@linux.com>

--- magic_test.go
+++ magic_test.go
@@ -1087,10 +1087,13 @@ func TestMagic_Separator(t *testing.T) {
 	n, _ := Version()
 
 	v := []string{"Bourne-Again shell script text executable", "a /bin/bash script, ASCII text executable", "data"}
-	if n < 524 || n > 533 {
-		// Older version of libmagic reports the results differently, but also latest version has been fixed
-		// so it does not try to report text files as "data" any more, albeit it returns an extra separator
-		// entry which causes an extra entry to be added.
+	if n < 524 {
+		// Older version of libmagic reports the results differently.
+		v = []string{"Bourne-Again shell script text executable", "a /bin/bash script, ASCII text executable"}
+	} else if n > 533 {
+		// Latest version of libmagic has been fixed so it does not try to
+		// report text files as "data" any more, albeit it returns an extra
+		// separator entry which causes an extra entry to be added.
 		v = []string{"Bourne-Again shell script text executable", "a /bin/bash script, ASCII text executable", ""}
 	}
 
